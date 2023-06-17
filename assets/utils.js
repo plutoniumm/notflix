ebb1b4d6e9781aa3f8e73c3d458c6d963cd96c5c
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2023-06-17 21:51:37 +0530

    es6 → es5

--- assets/utils.js
+++ assets/utils.js
@@ -1,26 +1,71 @@
-let dialog = document.getElementById( 'dialog' );
-if ( dialog )
-  fetch( '/list' ).then( res => res.json() )
-    .then( data => {
-      console.log( data );
-    } );
+"use strict";
 
-const handleKey = ( name, event ) => {
-  console.log( event );
-  fetch( `/track/${ name }`, {
+var dialog = document.getElementById( 'dialog' );
+if ( dialog ) fetch( '/list' ).then( function ( res ) {
+  return res.json();
+} ).then( function ( data ) {
+  console.log( data );
+} );
+
+function runCodeFunction ( num ) {
+  // seek 10s back
+  if ( num === 4 )
+    return player.currentTime( player.currentTime() - 10 );
+  // mock f5 for TV
+  if ( num === 5 )
+    return window.reload();
+  // seek 10s forward
+  if ( num === 6 )
+    return player.currentTime( player.currentTime() + 10 );
+}
+
+function handleKey ( name, event ) {
+  var key = event.key,
+    code = event.code;
+  fetch( "/track/" + name, {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+    headers: {
+      'Content-Type': 'application/json'
+    },
     body: JSON.stringify( {
       key: event.key,
       code: event.code,
       ctrlKey: event.ctrlKey,
       shiftKey: event.shiftKey,
-      altKey: event.altKey,
-    }
-    )
-  } ).catch( e => console.log( e ) );
+      altKey: event.altKey
+    } )
+  } ).catch( function ( e ) {
+    return console.log( e );
+  } );
+
+  if ( typeof player === "undefined" ) return 0;
+  if ( key === "MediaPlayPause" ) player.paused() ? player.play() : player.pause();
+  if ( code.startsWith( 'Digit' ) ) {
+    var num = parseInt( code.replace( 'Digit', '' ), 10 );
+    runCodeFunction( num );
+  }
+};
+function handleClick ( name, event ) {
+  var target = event.target;
+  fetch( "/track/".concat( name ), {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify( {
+      x: event.clientX,
+      y: event.clientY,
+      target: target.tagName + ":" + target.id
+    } )
+  } )[ "catch" ]( function ( e ) {
+    return console.log( e );
+  } );
 };
 
-window.addEventListener( 'keydown', e => handleKey( "keydown", e ) );
-window.addEventListener( 'keyup', e => handleKey( "keyup", e ) );
-window.addEventListener( 'keypress', e => handleKey( "keypress", e ) );
+
+window.addEventListener( 'keyup', function ( e ) {
+  return handleKey( "keyup", e );
+} );
+window.addEventListener( 'click', function ( e ) {
+  return handleClick( "click", e );
+} );
