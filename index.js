df988c7eb50fd9f44710b7a5eeb61361b534f7a5
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2023-06-17 06:53:59 +0530

    add tracker, vjs, styles, list, csp

--- index.js
+++ index.js
@@ -1,26 +1,50 @@
-import { createReadStream, statSync } from "fs";
+import { readdirSync, createReadStream, statSync, appendFile } from "fs";
 import { createServer } from 'http';
 import path, { resolve } from "path";
 
 const server = createServer( ( req, res ) => {
-  if ( req.method !== 'GET' && req.method !== 'HEAD' ) {
-    res.writeHead( 405 );
-    res.end( "method not allowed" );
-    return 0;
+  if ( req.method === 'GET' && req.url === "/" ) {
+    res.writeHead( 200, {
+      'Content-Type': 'text/html',
+      'Content-Security-Policy': "default-src *  data: 'unsafe-inline' 'unsafe-eval'; script-src * data: 'unsafe-inline' 'unsafe-eval'; img-src * data: 'unsafe-inline'; font-src * data: 'unsafe-inline' 'unsafe-eval'; worker-src * data: blob: 'unsafe-inline' 'unsafe-eval';"
+    } );
+    return createReadStream( resolve( "index.html" ) ).pipe( res );
   }
-  if ( req.url === "/" ) {
-    createReadStream( resolve( "index.html" ) ).pipe( res );
-    return 0;
+  if ( req.method === 'GET' && req.url.startsWith( "/assets" ) ) {
+    const file = req.url.replace( "/assets", "assets" );
+    console.log( file );
+    return createReadStream( resolve( file ) ).pipe( res )
+  };
+
+  if ( req.method === 'GET' && req.url.startsWith( "/track" ) ) {
+    // get data
+    const data = req.url.replace( "/track", "" );
+    console.log( data );
+    // write to file
+    appendFile( 'tests.txt', data, function ( err ) {
+      if ( err ) throw err;
+      console.log( 'Saved!' );
+    } );
   }
-  //if video content is requesting
-  if ( req.url === "/video" ) {
+
+
+  if ( req.method === 'GET' && req.url === "/list" ) {
+    const files =
+      readdirSync( resolve( "video" ) )
+        .filter( file => !file.startsWith( "." ) );
+    res.writeHead( 200, {
+      'Content-Type': 'application/json',
+    } );
+    return res.end( JSON.stringify( files ) );
+  }
+
+  if ( req.method === 'GET' && req.url === "/video" ) {
     const filepath = resolve( "video/video.mp4" );
-    const stat = statSync( filepath )
-    const fileSize = stat.size
-    const range = req.headers.range
+    const fileSize = statSync( filepath )?.size
+    const range = req.headers?.range
 
     if ( range ) {
-      //range format is "bytes=start-end",
+      // range format is "bytes=start-end",
       const parts = range.replace( /bytes=/, "" ).split( "-" );
 
       const start = parseInt( parts[ 0 ], 10 );
@@ -35,27 +59,23 @@ const server = createServer( ( req, res ) => {
       } catch ( error ) {
         console.log( error );
         res.writeHead( 400 );
-        res.end( "bad request" );
-        return 0;
+        return res.end( "bad request" );
       }
 
-      const head = {
+      /* 206 => partial content*/
+      res.writeHead( 206, {
         'Content-Range': `bytes ${ start }-${ end }/${ fileSize }`,
         'Accept-Ranges': 'bytes',
         'Content-Length': chunksize,
         'Content-Type': 'video/mp4',
-      }
-
-      /* 206 => partial content*/
-      res.writeHead( 206, head );
+      } );
       file.pipe( res );
     } else {
       // IF NO RANGE
-      const head = {
+      res.writeHead( 200, {
         'Content-Length': fileSize,
         'Content-Type': 'video/mp4',
-      }
-      res.writeHead( 200, head );
+      } );
       createReadStream( path ).pipe( res );
     }
   } else {
