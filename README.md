4171263ba0762c00bda08fcf48273c2bc52aab9a
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2023-06-17 18:44:47 +0530

    some improvements

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# hanflix
-MPEG-DASH experiments on Home Area Network
+<div align="center">
+<img src="./assets/tight.svg" height="150" alt="Home Area Network" />
+<h1>Notflix</h1>
+</div>
+
+Stream experiments on Home Area Network
--- index.js
+++ index.js
@@ -2,64 +2,69 @@ import { readdirSync, createReadStream, statSync, appendFile } from "fs";
 import { createServer } from 'http';
 import path, { resolve } from "path";
 
+const CSP = [
+  "default-src *  data: 'unsafe-inline' 'unsafe-eval';",
+  "script-src * data: 'unsafe-inline' 'unsafe-eval';",
+  "img-src * data: 'unsafe-inline';",
+  "font-src * data: 'unsafe-inline' 'unsafe-eval';",
+  "worker-src * data: blob: 'unsafe-inline' 'unsafe-eval'"
+].join( " " );
+
 const server = createServer( ( req, res ) => {
+  console.log( 14, req.url );
   if ( req.method === 'GET' && req.url === "/" ) {
     res.writeHead( 200, {
       'Content-Type': 'text/html',
-      'Content-Security-Policy': "default-src *  data: 'unsafe-inline' 'unsafe-eval'; script-src * data: 'unsafe-inline' 'unsafe-eval'; img-src * data: 'unsafe-inline'; font-src * data: 'unsafe-inline' 'unsafe-eval'; worker-src * data: blob: 'unsafe-inline' 'unsafe-eval';"
+      'Content-Security-Policy': CSP
     } );
     return createReadStream( resolve( "index.html" ) ).pipe( res );
   }
-  if ( req.method === 'POST' && req.url.startsWith( "/assets" ) ) {
+  if ( req.method === 'GET' && req.url.startsWith( "/assets" ) ) {
     const file = req.url.replace( "/assets", "assets" );
-    console.log( file );
+    console.log( 24, file );
     return createReadStream( resolve( file ) ).pipe( res )
   };
 
   if ( req.method === 'POST' && req.url.startsWith( "/track" ) ) {
-    const body = req.body;
-    const name = req.url.replace( "/track/", "" );
-    const data = `${ name } ${ body }\n`;
-    // write to file
-    appendFile( 'tests.txt', data, function ( err ) {
-      if ( err ) throw err;
-      console.log( 'Saved!' );
+    let body = '';
+    req.on( 'data', ( chunk ) => body += chunk );
+    req.on( 'end', () => {
+      console.log( 32, body );
+      appendFile( 'tests.txt', `${ req.url.replace( '/track/', '' ) } ${ body }\n`, console.log );
+
+      res.writeHead( 200, { 'Content-Type': 'text/html', } );
+      return res.end( "ok" );
     } );
   }
 
-
   if ( req.method === 'GET' && req.url === "/list" ) {
-    const files =
-      readdirSync( resolve( "video" ) )
-        .filter( file => !file.startsWith( "." ) );
-    res.writeHead( 200, {
-      'Content-Type': 'application/json',
-    } );
+    const files = readdirSync( resolve( "video" ) )
+      .filter( file => !file.startsWith( "." ) );
+    res.writeHead( 200, { 'Content-Type': 'application/json', } );
     return res.end( JSON.stringify( files ) );
   }
 
   if ( req.method === 'GET' && req.url === "/video" ) {
     const filepath = resolve( "video/video.mp4" );
-    const fileSize = statSync( filepath )?.size
-    const range = req.headers?.range
+    const fileSize = statSync( filepath )?.size;
+    const range = req.headers?.range;
 
     if ( range ) {
-      // range format is "bytes=start-end",
+      // format is "bytes=start-end",
       const parts = range.replace( /bytes=/, "" ).split( "-" );
 
       const start = parseInt( parts[ 0 ], 10 );
       const end = parts[ 1 ] ? parseInt( parts[ 1 ], 10 ) : fileSize - 1; //IF NO END => EOF
 
-      const chunksize = ( end - start ) + 1
-      /*we can provide offset values as options to
-     the fs.createReadStream to read part of content*/
+      const chunksize = ( end - start ) + 1;
       let file;
       try {
         file = createReadStream( filepath, { start, end } );
       } catch ( error ) {
         console.log( error );
+        console.log( "Bad Request @ /video" );
         res.writeHead( 400 );
-        return res.end( "bad request" );
+        return res.end( "Bad Request @ /video" );
       }
 
       /* 206 => partial content*/
@@ -80,8 +85,9 @@ const server = createServer( ( req, res ) => {
     }
   } else {
     // For when you dont know wtf happened
+    console.log( "Unkwnown Bad Request @ " + req.url );
     res.writeHead( 400 );
-    res.end( "bad request" );
+    res.end( "Unkwnown Bad Request @ " + req.url );
   }
 } )
 
